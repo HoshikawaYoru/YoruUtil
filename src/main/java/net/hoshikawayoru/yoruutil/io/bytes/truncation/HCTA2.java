524123cf7a08545150a651c9e3a6336c9e8166fe
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-06-02 18:15:37 +0800

    HCTA2

---
+++ src/main/java/net/hoshikawayoru/yoruutil/io/bytes/truncation/HCTA2.java
@@ -0,0 +1,25 @@
+package net.hoshikawayoru.yoruutil.io.bytes.truncation;
+
+public class HCTA2 {
+    public static byte[] truncation(byte[] bytes, int blockSize){
+        if (bytes == null){
+            return null;
+        }
+
+        if (blockSize >= bytes.length){
+            return bytes;
+        }
+
+
+        byte[] out = new byte[blockSize];
+        int blockCount = bytes.length / blockSize;
+
+        for (int i = 0;i < blockSize;i++){
+            for (int j = 0;j < (bytes.length / blockSize);j++){
+                out[i] = bytes[i * blockCount + j];
+            }
+        }
+
+        return out;
+    }
+}
