58e9d97b018e27a12053456b815918c4f907a156
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-06-15 16:34:35 +0800

    优化

---
+++ src/main/java/net/hoshikawayoru/yoru/util/io/file/FileReader.java
@@ -0,0 +1,30 @@
+package net.hoshikawayoru.yoru.util.io.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+public class FileReader {
+    public static final class bytes {
+        public static byte[] readAll(File file) throws IOException {
+            if (file == null){
+                return null;
+            }
+
+            InputStream input = Files.newInputStream(file.toPath());
+            byte[] bytes = new byte[0];
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = input.read(buffer)) > 0) {
+                byte[] newBytes = new byte[bytes.length + length];
+                System.arraycopy(bytes, 0, newBytes, 0, bytes.length);
+                System.arraycopy(buffer, 0, newBytes, bytes.length, length);
+                bytes = newBytes;
+            }
+            input.close();
+            return bytes;
+        }
+    }
+}
