ede62764ce4742b2a5f71227fbf91a30ab97a050
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-06-30 23:02:10 +0800

    优化

--- src/main/java/net/hoshikawayoru/minsed/util/file/FileUtil.java
+++ src/main/java/net/hoshikawayoru/minsed/util/file/FileUtil.java
@@ -6,24 +6,21 @@ import java.io.InputStream;
 import java.nio.file.Files;
 
 public class FileUtil {
-    public static final class bytes {
-        public static byte[] readAll(File file) throws IOException {
-            if (file == null){
-                return null;
-            }
-
-            InputStream input = Files.newInputStream(file.toPath());
-            byte[] bytes = new byte[0];
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = input.read(buffer)) > 0) {
-                byte[] newBytes = new byte[bytes.length + length];
-                System.arraycopy(bytes, 0, newBytes, 0, bytes.length);
-                System.arraycopy(buffer, 0, newBytes, bytes.length, length);
-                bytes = newBytes;
-            }
-            input.close();
-            return bytes;
+    public static byte[] readAllBytes(File file) throws IOException {
+        if (file == null){
+            return null;
         }
+        InputStream input = Files.newInputStream(file.toPath());
+        byte[] bytes = new byte[0];
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = input.read(buffer)) > 0) {
+            byte[] newBytes = new byte[bytes.length + length];
+            System.arraycopy(bytes, 0, newBytes, 0, bytes.length);
+            System.arraycopy(buffer, 0, newBytes, bytes.length, length);
+            bytes = newBytes;
+        }
+        input.close();
+        return bytes;
     }
 }
