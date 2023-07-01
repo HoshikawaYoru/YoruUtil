a6408de80289947e3d61c5cb3087d4f444cfcc67
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-07-01 22:27:37 +0800

    优化 检查索引是否超出数组索引

--- src/main/java/net/hoshikawayoru/minsed/util/bytes/ByteUtil.java
+++ src/main/java/net/hoshikawayoru/minsed/util/bytes/ByteUtil.java
@@ -166,6 +166,10 @@ public class ByteUtil {
         return out;
     }
     public static byte[] addByte(byte[] bytes, int index, byte b){
+        if (index < 0 || index > bytes.length - 1){
+            return null;
+        }
+
         byte[] out = new byte[bytes.length + 1];
         for (int i = 0,j = 0;i < bytes.length + 1;i++){
             if (i == index){
