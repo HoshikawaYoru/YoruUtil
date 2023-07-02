1b99af9ed609a2eaea3d1c94b16af20cb9936f4a
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-07-02 20:35:03 +0800

    新增 removeAll

--- src/net/hoshikawayoru/minsed/util/data/longer/LongerUtil.java
+++ src/net/hoshikawayoru/minsed/util/data/longer/LongerUtil.java
@@ -5,7 +5,6 @@ package net.hoshikawayoru.minsed.util.data.longer;
  */
 public class LongerUtil {
     /**
-     * 这个方法会返回long数组的所有元素的相加和
      * @param longs long数组
      * @return 相加后的long
      */
@@ -18,6 +17,24 @@ public class LongerUtil {
 
         for (long l : longs){
             out += l;
+        }
+
+        return out;
+    }
+
+    /**
+     * @param longs long数组
+     * @return 相减后的long
+     */
+    public static long removeAll(long[] longs){
+        if (longs == null){
+            return -1L;
+        }
+
+        long out = longs.clone()[0] * 2;
+
+        for (long l : longs){
+            out -= l;
         }
 
         return out;
