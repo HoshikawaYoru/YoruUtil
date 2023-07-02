12ed4ef371fee19b8d2ed8a11369e13888419266
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-07-02 21:58:21 +0800

    删除注释

--- src/main/java/net/hoshikawayoru/minsed/utilx/data/structure/Tuple.java
+++ src/main/java/net/hoshikawayoru/minsed/utilx/data/structure/Tuple.java
@@ -3,47 +3,25 @@ package net.hoshikawayoru.minsed.utilx.data.structure;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * 这个类是元组的实现
- *
- * @param <E> 元素
- */
+
 public class Tuple<E> {
     private ArrayList<E> arrayList = new ArrayList<>();
 
-    /**
-     * 元组是不可修改的.
-     *
-     * @param elements 元素
-     */
     public Tuple(E[] elements) {
         arrayList.addAll(Arrays.asList(elements));
     }
 
-    /**
-     * 通过索引获取元素
-     *
-     * @param index 索引
-     * @return 元素
-     */
+
     public E get(int index) {
         return arrayList.get(index);
     }
 
-    /**
-     * 返回元组的大小
-     *
-     * @return 元组的大小
-     */
+
     public int length() {
         return arrayList.size();
     }
 
-    /**
-     * 返回元组的字符串形式
-     *
-     * @return 元组的字符串形式
-     */
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
