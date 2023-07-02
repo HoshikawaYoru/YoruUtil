5d14ef5618b083f7a71ef874777e43139c4bcb38
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-07-02 22:50:23 +0800

    SandPane完整实现

--- src/main/java/net/hoshikawayoru/minsed/utilx/data/structure/SandPane.java
+++ src/main/java/net/hoshikawayoru/minsed/utilx/data/structure/SandPane.java
@@ -1,7 +1,52 @@
 package net.hoshikawayoru.minsed.utilx.data.structure;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class SandPane<E> {
-    private ArrayList<E> arrayList = new ArrayList<>();
+    private HashMap<E, E> hashMap = new HashMap<>();
+
+    public boolean isExist(E element){
+        if (hashMap.get(element) != null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void add(E element){
+        hashMap.put(element, element);
+    }
+
+    public void remove(E element){
+        hashMap.remove(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Collection<E> collection = hashMap.values();
+
+
+
+        stringBuilder.append("(");
+
+        int i = 0;
+
+        for (E element : collection){
+            if (i == collection.size() - 1){
+                stringBuilder.append(element);
+                stringBuilder.append(")");
+                continue;
+            }
+
+            stringBuilder.append(element);
+            stringBuilder.append(", ");
+
+            i++;
+        }
+
+        return stringBuilder.toString();
+    }
 }
