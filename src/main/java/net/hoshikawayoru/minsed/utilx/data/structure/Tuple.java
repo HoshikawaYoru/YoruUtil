af52b3015bd1c4b85d1e98166f188ce413bfe6ad
HoshikawaYoru <hoshikawa-yoru@outlook.com>
2023-07-02 21:07:39 +0800

    文档

--- src/main/java/net/hoshikawayoru/minsed/utilx/data/structure/Tuple.java
+++ src/main/java/net/hoshikawayoru/minsed/utilx/data/structure/Tuple.java
@@ -3,8 +3,17 @@ package net.hoshikawayoru.minsed.utilx.data.structure;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 这个类是元组的实现
+ * @param <E> 元素
+ */
 public class Tuple<E> {
     private ArrayList<E> arrayList = new ArrayList<>();
+
+    /**
+     * 元组是不可修改的.
+     * @param elements 元素
+     */
     public Tuple(E[] elements){
         arrayList.addAll(Arrays.asList(elements));
     }
---
+++ src/main/java/net/hoshikawayoru/minsed/utilx/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 这个包是扩展工具包.
+ * 提供了大量扩展工具和对象.
+ * 以便于使用.
+ */
+package net.hoshikawayoru.minsed.utilx;
